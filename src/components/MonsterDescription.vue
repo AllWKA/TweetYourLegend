7746083c5cc5088f0501e4b23ce87009b17b6a32
AllWKA <bryanjaramillobaldeon@gmail.com>
2020-09-12 14:29:26 +0100

    create monster description and monster image components

---
+++ src/components/MonsterDescription.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card-text>
+    <!-- level monster -->
+    <v-rating :value="monsterLevel" :color="colorLevel" dense half-increments readonly
+              :size="levelSize"></v-rating>
+    <!-- Monster Description -->
+    <div>{{ description }}</div>
+    <!-- Decision -->
+    <v-btn x-small v-for="(option, i) in options" :key="i">{{ option }}</v-btn>
+  </v-card-text>
+</template>
+<script>
+export default {
+  name: 'MonsterDescription',
+  props: {
+    monsterLevel: Number,
+    colorLevel: String,
+    levelSize: Number,
+    description: String,
+    options: Array
+  },
+  mounted() {
+  },
+  methods:{
+  }
+}
+</script>
---
+++ src/components/MonsterImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-img height="250" :src="imgURL">
+    <v-card-title style="color:black">{{title}}</v-card-title>
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: "MonsterImage",
+  props:{
+    imgURL: String,
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
